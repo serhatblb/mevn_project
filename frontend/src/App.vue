023ee7c7a35613cd8b9a2395565acbba6dbb558e
serhatblb <serhatblb23@gmail.com>
2024-02-23 00:34:19 +0300

    bootstarp, first component and fonts added

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<NavBar></NavBar>
+</template>
+
+<script>
+
+import NavBar from "@/components/NavBar.vue"
+
+export default {
+    name: "App",
+    components: {
+        NavBar
+    }
+}
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap');
+
+html, body {
+    font-family: "Mulish", sans-serif;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import {createApp} from "vue";
+import App from "@/App.vue"
+
+const app = createApp(App);
+
+app.mount("#app");
+
